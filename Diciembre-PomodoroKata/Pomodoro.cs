30f570fb5039cb50938b830ab4a707e9c799598e
Carlos Durban Antuñano <kaken20@gmail.com>
2012-07-23 12:41:25 +0200

    Comodo kata

---
+++ Diciembre-PomodoroKata/Pomodoro.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Diciembre_PomodoroKata
+{
+    public enum Estado : int
+    {
+        Parado = 1,
+        Terminado = 2,
+        Lanzado = 3
+
+    }
+
+    public class Pomodoro
+    {
+        public TimeSpan Duracion { get; private set; }
+        
+        public TimeSpan TiempoDisponible { get; private set; }
+
+        public Estado Estado { get; private set; }
+
+        public int Interrupciones { get; private set; }
+
+        protected Thread CountDown { get; private set; }
+
+        public Pomodoro()
+        {
+            InitPomodoro(new TimeSpan(0, 25, 0));
+        }   
+
+        public Pomodoro(TimeSpan duracion)
+        {
+            InitPomodoro(duracion);
+        }
+
+        protected void InitPomodoro(TimeSpan duracion)
+        {
+            Duracion = duracion;
+            Estado = Diciembre_PomodoroKata.Estado.Parado;
+            Interrupciones = 0;
+        }
+
+        public void Start()
+        {
+            TiempoDisponible = Duracion;
+
+            CountDown = new System.Threading.Thread(() => InitCount());
+
+            CountDown.Start();
+
+            Estado = Diciembre_PomodoroKata.Estado.Lanzado;
+        
+        }
+
+        protected void InitCount()
+        {
+            while (TiempoDisponible > new TimeSpan(0, 0, 0))
+            {
+                Estado = Diciembre_PomodoroKata.Estado.Lanzado;
+
+                TiempoDisponible = TiempoDisponible.Subtract(new TimeSpan(0, 0, 1));
+            }
+
+            Estado = Diciembre_PomodoroKata.Estado.Terminado;
+            CountDown = null;
+        }
+
+        public void Interrumpir()
+        {
+            Interrupciones += 1;
+            
+            if (CountDown != null)
+            {
+                CountDown.Interrupt();
+            }
+        }
+
+        public void Reiniciar()
+        {
+            this.TiempoDisponible = this.Duracion;
+            this.Interrupciones = 0;
+            Start();
+
+        }
+
+    }
+}
---
+++ Diciembre-PomodoroKataTest/PomodoroTest.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Diciembre_PomodoroKata;
+
+namespace Diciembre_PomodoroKataTest
+{
+    [TestFixture]
+    public class PomodoroTest
+    {
+
+        [Test]
+        public void Un_pomodoro_dura_25_minutos_defecto()
+        {
+            TimeSpan Duracion = new TimeSpan(0, 25, 0);
+
+            Pomodoro pomodoro = new Pomodoro();
+
+            Assert.AreEqual(Duracion, pomodoro.Duracion);
+        }
+
+        [Test]
+        public void Puedo_crear_un_pomodoro_con_cualquier_duracion()
+        {
+            TimeSpan Duracion = new TimeSpan(0, 30, 0);
+
+            Pomodoro pomodoro = new Pomodoro(Duracion);
+
+            Assert.AreEqual(Duracion, pomodoro.Duracion);
+        }
+
+        [Test]
+        public void Un_pomodoro_esta_parado_nada_mas_creado()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            Assert.AreEqual(Estado.Parado, pomodoro.Estado);
+        }
+
+        [Test]
+        public void Al_arrancar_el_pomodoro_la_cuenta_atras_comienza()
+        {
+
+            Pomodoro pomodoro = new Pomodoro(new TimeSpan(0, 0, 10));
+
+            pomodoro.Start();
+
+            Assert.IsTrue(pomodoro.TiempoDisponible <= new TimeSpan(0, 0, 10));
+        }
+
+        [Test]
+        public void Un_Pomodoro_termina_cuando_termina_su_tiempo()
+        {
+            Pomodoro pomodoro = new Pomodoro(new TimeSpan(0, 0, 1));
+
+            pomodoro.Start();
+
+            System.Threading.Thread.Sleep(1000);
+
+            Assert.AreEqual(pomodoro.Estado, Estado.Terminado);
+
+        }
+
+        [Test]
+        public void Un_Pomodoro_no_termina_hasta_que_no_se_agote_su_tiempo()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            pomodoro.Start();
+            
+            Assert.AreEqual(pomodoro.Estado, Estado.Lanzado);
+        }
+
+
+        [Test]
+        public void Un_pomodoro_se_inicia_sin_interupciones()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            Assert.AreEqual(0,pomodoro.Interrupciones);
+        }
+
+        [Test]
+        public void Un_pomodoro_sino_esta_iniciado_no_se_puede_interrumpir()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            pomodoro.Interrumpir();
+
+            Assert.AreEqual(Estado.Parado, pomodoro.Estado);
+        }
+
+        [Test]
+        public void Un_pomodoro_cuenta_las_interrupciones()
+        {
+
+            Pomodoro pomodoro = new Pomodoro();
+
+            pomodoro.Start();
+
+            pomodoro.Interrumpir();
+
+            Assert.AreEqual(1, pomodoro.Interrupciones);
+            
+        }
+
+        [Test]
+        public void Un_pomodoro_arrancado_se_reinicia()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            pomodoro.Start();
+
+            pomodoro.Reiniciar();
+
+            Assert.AreEqual(new TimeSpan(0, 25, 0), pomodoro.Duracion);
+        }
+
+        [Test]
+        public void Un_pomodo_se_reinicia_sin_interrupciones()
+        {
+            Pomodoro pomodoro = new Pomodoro();
+
+            pomodoro.Start();
+
+            pomodoro.Reiniciar();
+
+            Assert.AreEqual(0, pomodoro.Interrupciones);
+        }
+
+
+
+    
+    }
+}
